4ca89c7cab5d79a0eb616473b08ac51c54b10298
adam aulia rahmadi <adamaulia242@gmail.com>
2015-04-23 22:28:06 +0700

    gui front end dikit lagi jadi

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'emptystring';
+$db['default']['password'] = '';
 $db['default']['database'] = 'rploot';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
